0ec84e239cf9a26c40b1cae3a9b9e1bdb794d0cd
Matthew Yee-King <matthew@yeeking.net>
2025-06-12 13:40:46 +0100

    adding test code and midi gen test python

---
+++ src/tests/unit-tests.cpp
@@ -0,0 +1,60 @@
+#include <JuceHeader.h>
+
+
+#include "../plugin/Transcriber.h"
+// #include "tinywav/myk_tiny.h"
+
+
+class BasicTest : public juce::UnitTest
+{
+    public:
+    BasicTest() : juce::UnitTest("BasicTest", "BasicTest") {}
+
+    void runTest() override
+    {
+        {
+            beginTest("If six was nine");
+            expectNotEquals(6, 9);
+
+        }
+    }
+};
+
+
+class NoteExtractTest : public juce::UnitTest
+{
+    public:
+    NoteExtractTest() : juce::UnitTest("NoteExtractTest", "NoteExtractTest") {}
+
+    void runTest() override
+    {
+        {
+            Transcriber transcriber;
+
+            // transcriber.storeAudio();
+
+            beginTest("transcribe");
+            expectNotEquals(6, 9);
+
+        }
+    }
+};
+
+
+int main()
+{
+    
+    std::cout << "Tests main function " << std::endl;
+ 
+
+    juce::UnitTestRunner runner;
+
+    BasicTest basicTest;
+    NoteExtractTest noteExtractTest;
+
+    runner.runTestsInCategory("BasicTest");
+    runner.runTestsInCategory("NoteExtractTest");
+    
+
+    return 0;
+}
